d9a1ce0c48c537d325191b69ffb198779dfaac40
IT22365378 <127436562+IT22365378@users.noreply.github.com>
2023-03-11 18:31:29 +0000

    cording

--- Tute04.c
+++ Tute04.c
@@ -5,15 +5,49 @@ Implement the three functions minimum(), maximum() and multiply() below the main
 Do not change the code given in the main() function when you are implementing your solution.*/
 
 #include <stdio.h>
+int minimum(int n1, int n2);     //declare minimum funtion
+int maximum(int n1, int n2);     //declare maximum funtion
+int multiply(int n1, int n2);    //declare multiply funtion
 
 int main() {
    int no1, no2;
-   printf("Enter a value for no 1 : ");
+   printf("Enter a value for no 1 : ");       //for input value
    scanf("%d", &no1);
-   printf("Enter a value for no 2 : ");
+   printf("Enter a value for no 2 : ");       //for input value
    scanf("%d", &no2);
-   printf("%d ", minimum(no1, no2));
-   printf("%d ", maximum(no1, no2));
-   printf("%d ", multiply(no1, no2));
+   printf("%d ", minimum(no1, no2));          //calling funtion
+   printf("%d ", maximum(no1, no2));          //calling funtion
+   printf("%d ", multiply(no1, no2));         //calling funtion
    return 0;
+}
+
+int minimum(int n1, int n2)       //impliment the minimum funtion
+{
+
+  if(n1 > n2)
+  {
+    return n2;
+  }
+  else{
+    return n1;
+  }
+}
+
+int maximum(int n1, int n2)       //impliment the maximum funtion
+{
+  if(n1 > n2)
+  {
+    return n1;
+  }
+  else{
+    return n2;
+  }
+}
+
+int multiply(int n1, int n2)     //impliment the multiply funtion
+{
+  int answer;
+  answer = n1 * n2;
+
+  return answer;
 }
